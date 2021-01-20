919a43e02e3bc6d29e356e255d76e25edae36395
Jan Pekar <jan.pekar@imatic.cz>
2021-01-20 16:57:29 +0100

    Fixed homedir path from env

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,6 @@
 getent passwd "$USER" >/dev/null || useradd "$USER" --uid ${UID:-1000} -s /bin/bash
 #Copy skeleton files and set proper permissions
 /sbin/mkhomedir_helper "${USER}" 027
-chown "$USER" -R /home/max
+chown "$USER" -R /home/"${USER}"
 #Run under created user
 su - "$USER" -c "DISPLAY=$DISPLAY max_home_automation" 
